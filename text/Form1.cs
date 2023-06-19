783b72dfe7fd1a4951b3a640bbc868b01a808971
Katsiev <118531597+Katsiev@users.noreply.github.com>
2023-06-19 13:13:09 +0300

    Add files via upload

---
+++ text/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Drawing.Printing;
+
+namespace text
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) //Проверяем был ли выбран файл
+            {
+                richTextBox1.Clear(); //Очищаем richTextBox
+                openFileDialog1.Filter = "Text Files (*.txt)|*.txt";
+                string fileName = openFileDialog1.FileName;
+                richTextBox1.Text = File.ReadAllText(fileName, Encoding.GetEncoding(1251));
+            }
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+           saveFileDialog1.Filter = "Text Files|*.txt";//Задаем доступные расширения
+           saveFileDialog1.DefaultExt = ".txt"; //Задаем расширение по умолчанию
+            
+           if (saveFileDialog1.ShowDialog() == DialogResult.OK) //Проверяем подтверждение
+             
+{
+           var name = saveFileDialog1.FileName; //Задаем имя файлу
+           File.WriteAllText(name, richTextBox1.Text, Encoding.GetEncoding(1251)); //Записываем
+           
+}
+           richTextBox1.Clear();
+
+        }
+
+        private void выравниваниеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            openFileDialog1.FileName = @"data\Text2.txt";
+            openFileDialog1.Filter =
+                     "Текстовые файлы (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.Filter =
+                     "Текстовые файлы (*.txt)|*.txt|All files (*.*)|*.*";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.GetText() != "")
+            {
+                richTextBox1.SelectedText = Clipboard.GetText();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectedText != "")
+            {
+                Clipboard.SetText(richTextBox1.SelectedText);
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectedText != "")
+            {
+                Clipboard.SetText(richTextBox1.SelectedText);
+                richTextBox1.SelectedText = "";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Select();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.ForeColor = colorDialog.Color;
+            }
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) //Проверяем был ли выбран файл
+            {
+                richTextBox1.Clear(); //Очищаем richTextBox
+                openFileDialog1.Filter = "Text Files (*.txt)|*.txt";
+                string fileName = openFileDialog1.FileName;
+                richTextBox1.Text = File.ReadAllText(fileName, Encoding.GetEncoding(1251));
+            }
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) //Проверяем был ли выбран файл
+            {
+                richTextBox1.Clear(); //Очищаем richTextBox
+                openFileDialog1.Filter = "Text Files (*.txt)|*.txt";
+                string fileName = openFileDialog1.FileName;
+                richTextBox1.Text = File.ReadAllText(fileName, Encoding.GetEncoding(1251));
+            }
+        }
+
+        private void вырезатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectedText != "")
+            {
+                Clipboard.SetText(richTextBox1.SelectedText);
+                richTextBox1.SelectedText = "";
+            }
+        }
+
+        private void копироатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectedText != "")
+            {
+                Clipboard.SetText(richTextBox1.SelectedText);
+            }
+        }
+
+        private void вставитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.GetText() != "")
+            {
+                richTextBox1.SelectedText = Clipboard.GetText();
+            }
+        }
+
+        private void очиститьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            openFileDialog1.FileName = @"data\Text2.txt";
+            openFileDialog1.Filter =
+                     "Текстовые файлы (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.Filter =
+                     "Текстовые файлы (*.txt)|*.txt|All files (*.*)|*.*";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Font = fontDialog.Font;
+            }
+        }
+
+        private void шрифтToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Font = fontDialog.Font;
+            }
+        }
+
+        private void цветToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.ForeColor = colorDialog.Color;
+            }
+        }
+
+        private void поЦентруToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Select(); // выравнивание только выделенного текста
+                                   //richTextBox1.SelectAll(); //выделение всего текста
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+            //richTextBox1.DeselectAll(); //Отмена выделения
+        }
+
+        private void поПравойСторонеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Select(); // выравнивание только выделенного текста
+                                   //richTextBox1.SelectAll(); //выделение всего текста
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Right;
+            //richTextBox1.DeselectAll(); //Отмена выделения
+        }
+
+        private void поЛевойСторонеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Select(); // выравнивание только выделенного текста
+                                   //richTextBox1.SelectAll(); //выделение всего текста
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Left;
+            //richTextBox1.DeselectAll(); //Отмена выделения
+        }
+
+        private void печатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void предварительныйПросмотрToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
